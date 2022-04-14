ba442a46c21ff087711a41e9d55e63021d13e04f
Alisaeed090 <saeedali090@gmail.com>
2022-04-15 03:17:49 +0500

    updatedCode

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var httpsOptions = {
   cert: fs.readFileSync('./fake-keys/certificate.pem')
 };
 let isLocal = process.env.PORT == null;
-var serverPort = (process.env.PORT  || 4443);
+var serverPort = (process.env.PORT || 4443);
 var server = null;
 if (isLocal) {
   server = require('https').createServer(httpsOptions, app);
@@ -27,12 +27,12 @@ var io = require('socket.io')(server);
 let socketIdToNames = {};
 //------------------------------------------------------------------------------
 //  Serving static files
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   console.log('get /');
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/draw', function(req, res){
+app.get('/draw', function (req, res) {
   console.log('get /');
   res.sendFile(__dirname + '/draw.html');
 });
@@ -41,7 +41,7 @@ app.get('/draw', function(req, res){
 // app.use('/script', express.static(path.join(__dirname, 'script')));
 // app.use('/image', express.static(path.join(__dirname, 'image')));
 
-server.listen(serverPort, function(){
+server.listen(serverPort, function () {
   console.log('Rewebrtc-server is up and running at %s port', serverPort);
   if (isLocal) {
     open('https://localhost:' + serverPort)
@@ -50,9 +50,14 @@ server.listen(serverPort, function(){
 
 //------------------------------------------------------------------------------
 //  WebRTC Signaling
-function socketIdsInRoom(roomId) {
-  var socketIds = io.nsps['/'].adapter.rooms[roomId];
-  if (socketIds) {
+function socketIdsInRoom(socketRoomID, socketId) {
+  // var socketIds = io.nsps['/'].adapter.rooms[roomId];
+  const clients = io.sockets.adapter.rooms.get(socketRoomID);
+  let arr = Array.from(clients)
+  arr = arr.filter((id) => id != socketId)
+  console.log({ arr })
+  return  arr
+  if (arr) {
     var collection = [];
     for (var key in socketIds) {
       collection.push(key);
@@ -63,9 +68,9 @@ function socketIdsInRoom(roomId) {
   }
 }
 
-io.on('connection', function(socket){
-  console.log('Connection');
-  socket.on('disconnect', function(){
+io.on('connection', function (socket) {
+  console.log('Connection', { socket: socket.id });
+  socket.on('disconnect', function () {
     console.log('Disconnect');
     delete socketIdToNames[socket.id];
     if (socket.room) {
@@ -78,41 +83,43 @@ io.on('connection', function(socket){
   /**
    * Callback: list of {socketId, name: name of user}
    */
-  socket.on('join', function(joinData, callback){ //Join room
+  socket.on('join', function (joinData, callback) { //Join room
     console.log('join');
-    let roomId = joinData.roomId;
+    let socketRoomID = joinData.socketRoomID;
+    const uid =  joinData.uid
     let name = joinData.name;
-    socket.join(roomId);
-    socket.room = roomId;
-    socketIdToNames[socket.id] = name;
+    socket.join(socketRoomID);
+    socket.room = socketRoomID;
+    socketIdToNames[socket.id] = uid;
 
-    
-    var socketIds = socketIdsInRoom(roomId);
+
+    var socketIds = socketIdsInRoom(socketRoomID, socket.id);
     let friends = socketIds.map((socketId) => {
       return {
         socketId: socketId,
-        name: socketIdToNames[socketId]
+        uid: socketIdToNames[socketId]
       }
     }).filter((friend) => friend.socketId != socket.id);
     callback(friends);
     //broadcast
     friends.forEach((friend) => {
-      io.sockets.connected[friend.socketId].emit("join", {
-        socketId: socket.id, name
+      socket.to(friend.socketId).emit("join", {
+        socketId: socket.id,
       });
     });
     console.log('Join: ', joinData);
   });
 
-  socket.on('exchange', function(data){
+  socket.on('exchange', function (data) {
     console.log('exchange', data);
     data.from = socket.id;
-    var to = io.sockets.connected[data.to];
-    to.emit('exchange', data);
+    // var to = io.sockets.connected[data.to];
+    // to.emit('exchange', data);
+    socket.to(data.to).emit("exchange", data);
   });
 
-  socket.on("count", function(roomId, callback) {
-    var socketIds = socketIdsInRoom(roomId);
+  socket.on("count", function (socketRoomID, callback) {
+    var socketIds = socketIdsInRoom(socketRoomID);
     callback(socketIds.length);
   });
 
