7e75d773c6eafda8e783b9fd39c54c26d3803716
Alisaeed090 <saeedali090@gmail.com>
2022-05-23 22:54:53 +0500

    ssss

--- app.js
+++ app.js
@@ -80,64 +80,28 @@ app.post("/PushNotification", jsonParser, (request, response) => {
 
   const firestore = admin.firestore();
   let participants = request.body.data.participants;
-  // var options = {
-  //   playSound: true, // (optional) default: true
-  //   soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-  //   vibrate: true, // (optional) default: true
-  //   vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-  //   importance: 'high', // (optional) set notification importance, default: high
-  //   allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
-  //   ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear)
-  //   priority: 'high',
-  //   timeToLive: 60 * 10,
-  //   content_available: true
-  // };
-  // const payload = {
-  //   notification: {
-  //     title: request.body.title,
-  //     body: request.body.desc,
-  //     icon: ' ic_notification',
-  //     sound: 'default',
-  //     content_available: "1"
-  //   },
-
-  //   data: {
-  //     participants: JSON.stringify(participants),
-  //     allParticipants: JSON.stringify(request.body.data.allParticipants),
-  //     sender: request.body.data.sender,
-  //     callRoomID: request.body.data.callRoomID,
-  //     roomTitle: request.body.data.roomTitle,
-  //     callType: request.body.data.callType,
-  //     roomTitle: request.body.data.roomTitle,
-  //     groupID: request.body.data.groupID,
-  //     roomImage: request.body.data.roomImage,
-  //     roomType: request.body.data.roomType
-
-  //   },
-  // };
-  const newData = {
-    title: request.body.title,
-    body: request.body.desc,
-    participants: JSON.stringify(participants),
-    allParticipants: JSON.stringify(request.body.data.allParticipants),
-    sender: request.body.data.sender,
-    callRoomID: request.body.data.callRoomID,
-    roomTitle: request.body.data.roomTitle,
-    callType: request.body.data.callType,
-    roomTitle: request.body.data.roomTitle,
-    groupID: request.body.data.groupID,
-    roomImage: request.body.data.roomImage,
-    roomType: request.body.data.roomType
-
-  }
-  var newPayload = {
+  var options = {
+    playSound: true, // (optional) default: true
+    soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+    vibrate: true, // (optional) default: true
+    vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+    importance: 'high', // (optional) set notification importance, default: high
+    allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+    ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear)
+    priority: 'high',
+    timeToLive: 60 * 10,
+    content_available: true
+  };
+  const payload = {
     notification: {
       title: request.body.title,
       body: request.body.desc,
+      icon: ' ic_notification',
+      sound: 'default',
+      content_available: "1"
     },
+
     data: {
-      title: request.body.title,
-      body: request.body.desc,
       participants: JSON.stringify(participants),
       allParticipants: JSON.stringify(request.body.data.allParticipants),
       sender: request.body.data.sender,
@@ -148,98 +112,135 @@ app.post("/PushNotification", jsonParser, (request, response) => {
       groupID: request.body.data.groupID,
       roomImage: request.body.data.roomImage,
       roomType: request.body.data.roomType
-    },
-    apns: {
-      payload: {
-        aps: {
-          sound: "default",
-        },
-      },
+
     },
   };
+  // const newData = {
+  //   title: request.body.title,
+  //   body: request.body.desc,
+  //   participants: JSON.stringify(participants),
+  //   allParticipants: JSON.stringify(request.body.data.allParticipants),
+  //   sender: request.body.data.sender,
+  //   callRoomID: request.body.data.callRoomID,
+  //   roomTitle: request.body.data.roomTitle,
+  //   callType: request.body.data.callType,
+  //   roomTitle: request.body.data.roomTitle,
+  //   groupID: request.body.data.groupID,
+  //   roomImage: request.body.data.roomImage,
+  //   roomType: request.body.data.roomType
 
-  console.log({newData })
+  // }
+  // var newPayload = {
+  //   notification: {
+  //     title: request.body.title,
+  //     body: request.body.desc,
+  //   },
+  //   data: {
+  //     title: request.body.title,
+  //     body: request.body.desc,
+  //     participants: JSON.stringify(participants),
+  //     allParticipants: JSON.stringify(request.body.data.allParticipants),
+  //     sender: request.body.data.sender,
+  //     callRoomID: request.body.data.callRoomID,
+  //     roomTitle: request.body.data.roomTitle,
+  //     callType: request.body.data.callType,
+  //     roomTitle: request.body.data.roomTitle,
+  //     groupID: request.body.data.groupID,
+  //     roomImage: request.body.data.roomImage,
+  //     roomType: request.body.data.roomType
+  //   },
+  //   apns: {
+  //     payload: {
+  //       aps: {
+  //         sound: "default",
+  //       },
+  //     },
+  //   },
+  // };
+
+  // console.log({newData })
   Promise.all(participants)
     .then(async () => {
       for (const eachUserUid of participants) {
         const user = await firestore.collection("Users").doc(eachUserUid.uid).get()
         console.log({ user: JSON.stringify(user) })
         if (user.data().tokens) {
-          newPayload.tokens = user.data().tokens; //Needed for sendMulticast
+          // newPayload.tokens = user.data().tokens; //Needed for sendMulticast
           
-          admin.messaging().sendMulticast(newPayload)
-            .then((response) => {
-              console.log(response.successCount + ' messages were sent successfully');
-            });
+          // admin.messaging().sendMulticast(newPayload)
+          //   .then((response) => {
+          //     console.log(response.successCount + ' messages were sent successfully');
+          //   });
 
-          // user.data().tokens.forEach((token) => {
-            // admin
-            //   .messaging()
-            //   .sendToDevice(token, payload, options)
-            //   .then(function (res) {
-            //     console.log('Succesfully sent message Group', request.body.title + "to " + user.data().userName, { token });
-
-            //   })
-            //   .catch(function (error) {
-            //     // response.json(error)
-            //     console.log(error, 'Error sending message');
-            //   });
-
-
-          // })
-        }
-
-        if (user.data().pushKitIosToken) {
-          if (request.body.title === "Incomming Call") {
-            user.data().pushKitIosToken.forEach(async (token) => {
-
-              if (token.length > 0) {
-                // sendpushKitIosCallNotification(token, request.body.data)
-                let data = request.body.data
-                const payload = {
-                  callerName: "Wepoc",
-                  // handle: recipient.email || recipient.phone || recipient.phoneNumber,
-                  handle: "wepoc",
-                  callRoomID: data.callRoomID,
-                  uuid: uuidv4(),
-                  chatType: data.callType,
-                  roomTitle: data.roomTitle,
-                  roomImage: data.roomImage,
-
-                };
-
-
-
-                const notification = new apn.Notification({
-                  aps: {
-                    "content-available": 1,
-                    "content-available": true
+          user.data().tokens.forEach((token) => {
+            admin
+              .messaging()
+              .sendToDevice(token, payload, options)
+              .then(function (res) {
+                console.log('Succesfully sent message Group', request.body.title + "to " + user.data().userName, { token });
+                if (user.data().pushKitIosToken) {
+                  if (request.body.title === "Incomming Call") {
+                    user.data().pushKitIosToken.forEach(async (token) => {
+        
+                      if (token.length > 0) {
+                        // sendpushKitIosCallNotification(token, request.body.data)
+                        let data = request.body.data
+                        const payload = {
+                          callerName: "Wepoc",
+                          // handle: recipient.email || recipient.phone || recipient.phoneNumber,
+                          handle: "wepoc",
+                          callRoomID: data.callRoomID,
+                          uuid: uuidv4(),
+                          chatType: data.callType,
+                          roomTitle: data.roomTitle,
+                          roomImage: data.roomImage,
+        
+                        };
+        
+        
+        
+                        const notification = new apn.Notification({
+                          aps: {
+                            "content-available": 1,
+                            "content-available": true
+                          }
+                        });
+        
+                        const recepients = [];
+                        recepients.push(apn.token(token));
+        
+                        // notification.topic = topic ? topic + '.voip' : 'io.instamobile.chat.swift.voip'; // you have to add the .voip suffix here!!
+                        notification.topic = "com.chat.wepoc.voip"
+                        notification.payload = payload;
+        
+        
+        
+                        apnProvider.send(notification, recepients).then((reponse) => {
+                          // console.log("Send push notifications to " + token + " topic: ", "payload", payload);
+                          console.log({ pushKitIosCallNotification: JSON.stringify(notification) });
+        
+        
+        
+                        });
+        
+        
+                      }
+        
+                    })
                   }
-                });
+                }
 
-                const recepients = [];
-                recepients.push(apn.token(token));
-
-                // notification.topic = topic ? topic + '.voip' : 'io.instamobile.chat.swift.voip'; // you have to add the .voip suffix here!!
-                notification.topic = "com.chat.wepoc.voip"
-                notification.payload = payload;
-
+              })
+              .catch(function (error) {
+                // response.json(error)
+                console.log(error, 'Error sending message');
+              });
 
 
-                apnProvider.send(notification, recepients).then((reponse) => {
-                  // console.log("Send push notifications to " + token + " topic: ", "payload", payload);
-                  console.log({ pushKitIosCallNotification: JSON.stringify(notification) });
-
-
-
-                });
-
-
-              }
-
-            })
-          }
+          })
         }
+
+  
       }
 
 
