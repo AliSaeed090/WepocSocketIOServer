ecbba99893ee82307181c0cfcb54d63b0be1362c
Alisaeed090 <saeedali090@gmail.com>
2022-05-23 22:43:31 +0500

    ss

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const apn = require('apn');
 const admin = require("firebase-admin");
+ 
+//  const { getMessaging } = require('firebase-admin/messaging');
 var serviceAccount = require('./wepoc-446d9-firebase-adminsdk-flhzi-2aa00339a8.json');
 var bodyParser = require('body-parser')
 // create application/json parser
 var jsonParser = bodyParser.json()
- 
+
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 admin.initializeApp({
@@ -72,36 +74,72 @@ server.listen(serverPort, function () {
 
 
 app.post("/PushNotification", jsonParser, (request, response) => {
-  console.log({request:request.body})
+  console.log({ request: request.body })
   //code to perform particular action.
   //To access POST variable use req.body()methods.
 
   const firestore = admin.firestore();
   let participants = request.body.data.participants;
-  var options = {
-    playSound: true, // (optional) default: true
-    soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-    vibrate: true, // (optional) default: true
-    vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-    importance: 'high', // (optional) set notification importance, default: high
-    allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
-    ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear)
-    priority: 'high',
-    timeToLive: 60 * 10,
-    content_available: true
-  };
-  const payload = {
+  // var options = {
+  //   playSound: true, // (optional) default: true
+  //   soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+  //   vibrate: true, // (optional) default: true
+  //   vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+  //   importance: 'high', // (optional) set notification importance, default: high
+  //   allowWhileIdle: false, // (optional) set notification to work while on doze, default: false
+  //   ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear)
+  //   priority: 'high',
+  //   timeToLive: 60 * 10,
+  //   content_available: true
+  // };
+  // const payload = {
+  //   notification: {
+  //     title: request.body.title,
+  //     body: request.body.desc,
+  //     icon: ' ic_notification',
+  //     sound: 'default',
+  //     content_available: "1"
+  //   },
+
+  //   data: {
+  //     participants: JSON.stringify(participants),
+  //     allParticipants: JSON.stringify(request.body.data.allParticipants),
+  //     sender: request.body.data.sender,
+  //     callRoomID: request.body.data.callRoomID,
+  //     roomTitle: request.body.data.roomTitle,
+  //     callType: request.body.data.callType,
+  //     roomTitle: request.body.data.roomTitle,
+  //     groupID: request.body.data.groupID,
+  //     roomImage: request.body.data.roomImage,
+  //     roomType: request.body.data.roomType
+
+  //   },
+  // };
+  const newData = {
+    title: request.body.title,
+    body: request.body.desc,
+    participants: JSON.stringify(participants),
+    allParticipants: JSON.stringify(request.body.data.allParticipants),
+    sender: request.body.data.sender,
+    callRoomID: request.body.data.callRoomID,
+    roomTitle: request.body.data.roomTitle,
+    callType: request.body.data.callType,
+    roomTitle: request.body.data.roomTitle,
+    groupID: request.body.data.groupID,
+    roomImage: request.body.data.roomImage,
+    roomType: request.body.data.roomType
+
+  }
+  var newPayload = {
     notification: {
       title: request.body.title,
       body: request.body.desc,
-      icon: ' ic_notification',
-      sound: 'default',
-      content_available: "1"
     },
-
     data: {
+      title: request.body.title,
+      body: request.body.desc,
       participants: JSON.stringify(participants),
-      allParticipants:JSON.stringify(request.body.data.allParticipants),
+      allParticipants: JSON.stringify(request.body.data.allParticipants),
       sender: request.body.data.sender,
       callRoomID: request.body.data.callRoomID,
       roomTitle: request.body.data.roomTitle,
@@ -110,31 +148,45 @@ app.post("/PushNotification", jsonParser, (request, response) => {
       groupID: request.body.data.groupID,
       roomImage: request.body.data.roomImage,
       roomType: request.body.data.roomType
-
+    },
+    apns: {
+      payload: {
+        aps: {
+          sound: "default",
+        },
+      },
     },
   };
 
-  console.log({participants, length:participants.length})
+  console.log({newData })
   Promise.all(participants)
     .then(async () => {
       for (const eachUserUid of participants) {
         const user = await firestore.collection("Users").doc(eachUserUid.uid).get()
         console.log({ user: JSON.stringify(user) })
         if (user.data().tokens) {
+          newPayload.tokens = user.data().tokens; //Needed for sendMulticast
+          
+          admin.messaging().sendMulticast(newPayload)
+            .then((response) => {
+              console.log(response.successCount + ' messages were sent successfully');
+            });
 
-          user.data().tokens.forEach((token) => {
-            admin
-              .messaging()
-              .sendToDevice(token, payload, options)
-              .then(function (res) {
-                console.log('Succesfully sent message Group', request.body.title + "to " + user.data().userName, { token });
-               
-              })
-              .catch(function (error) {
-                // response.json(error)
-                console.log(error, 'Error sending message');
-              });
-          })
+          // user.data().tokens.forEach((token) => {
+            // admin
+            //   .messaging()
+            //   .sendToDevice(token, payload, options)
+            //   .then(function (res) {
+            //     console.log('Succesfully sent message Group', request.body.title + "to " + user.data().userName, { token });
+
+            //   })
+            //   .catch(function (error) {
+            //     // response.json(error)
+            //     console.log(error, 'Error sending message');
+            //   });
+
+
+          // })
         }
 
         if (user.data().pushKitIosToken) {
